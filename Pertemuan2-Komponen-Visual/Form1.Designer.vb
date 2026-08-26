3a0634ddb141db9a9d266765f2d43306b7b143ac
Gilbeck <grael1302@gmail.com>
2026-08-26 09:53:23 +0700

    Pertemuan 2 - Komponen Visual

---
+++ Pertemuan2-Komponen-Visual/Form1.Designer.vb
@@ -0,0 +1,141 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        lblNama = New Label()
+        txtNama = New TextBox()
+        lblNIM = New Label()
+        txtNIM = New TextBox()
+        lblKom = New Label()
+        txtKom = New TextBox()
+        btnTampilkan = New Button()
+        btnHapus = New Button()
+        btnKeluar = New Button()
+        SuspendLayout()
+        ' 
+        ' lblNama
+        ' 
+        lblNama.AutoSize = True
+        lblNama.Location = New Point(166, 15)
+        lblNama.Name = "lblNama"
+        lblNama.Size = New Size(56, 20)
+        lblNama.TabIndex = 0
+        lblNama.Text = "Nama :"
+        ' 
+        ' txtNama
+        ' 
+        txtNama.Location = New Point(234, 12)
+        txtNama.Name = "txtNama"
+        txtNama.Size = New Size(125, 27)
+        txtNama.TabIndex = 1
+        ' 
+        ' lblNIM
+        ' 
+        lblNIM.AutoSize = True
+        lblNIM.Location = New Point(166, 55)
+        lblNIM.Name = "lblNIM"
+        lblNIM.Size = New Size(44, 20)
+        lblNIM.TabIndex = 2
+        lblNIM.Text = "NIM :"
+        ' 
+        ' txtNIM
+        ' 
+        txtNIM.Location = New Point(234, 52)
+        txtNIM.Name = "txtNIM"
+        txtNIM.Size = New Size(125, 27)
+        txtNIM.TabIndex = 3
+        ' 
+        ' lblKom
+        ' 
+        lblKom.AutoSize = True
+        lblKom.Location = New Point(166, 97)
+        lblKom.Name = "lblKom"
+        lblKom.Size = New Size(47, 20)
+        lblKom.TabIndex = 4
+        lblKom.Text = "Kom :"
+        ' 
+        ' txtKom
+        ' 
+        txtKom.Location = New Point(234, 94)
+        txtKom.Name = "txtKom"
+        txtKom.Size = New Size(125, 27)
+        txtKom.TabIndex = 5
+        ' 
+        ' btnTampilkan
+        ' 
+        btnTampilkan.Location = New Point(166, 140)
+        btnTampilkan.Name = "btnTampilkan"
+        btnTampilkan.Size = New Size(94, 29)
+        btnTampilkan.TabIndex = 6
+        btnTampilkan.Text = "Tampilkan"
+        btnTampilkan.UseVisualStyleBackColor = True
+        ' 
+        ' btnHapus
+        ' 
+        btnHapus.Location = New Point(289, 140)
+        btnHapus.Name = "btnHapus"
+        btnHapus.Size = New Size(94, 29)
+        btnHapus.TabIndex = 7
+        btnHapus.Text = "Hapus"
+        btnHapus.UseVisualStyleBackColor = True
+        ' 
+        ' btnKeluar
+        ' 
+        btnKeluar.Location = New Point(225, 184)
+        btnKeluar.Name = "btnKeluar"
+        btnKeluar.Size = New Size(94, 29)
+        btnKeluar.TabIndex = 8
+        btnKeluar.Text = "Keluar"
+        btnKeluar.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(574, 282)
+        Controls.Add(btnKeluar)
+        Controls.Add(btnHapus)
+        Controls.Add(btnTampilkan)
+        Controls.Add(txtKom)
+        Controls.Add(lblKom)
+        Controls.Add(txtNIM)
+        Controls.Add(lblNIM)
+        Controls.Add(txtNama)
+        Controls.Add(lblNama)
+        Name = "Form1"
+        Text = "Profile Mahasiswa"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents lblNama As Label
+    Friend WithEvents txtNama As TextBox
+    Friend WithEvents lblNIM As Label
+    Friend WithEvents txtNIM As TextBox
+    Friend WithEvents lblKom As Label
+    Friend WithEvents txtKom As TextBox
+    Friend WithEvents btnTampilkan As Button
+    Friend WithEvents btnHapus As Button
+    Friend WithEvents btnKeluar As Button
+
+End Class
