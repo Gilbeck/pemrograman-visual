e86f7c59e3ee6b3dfbdcb07591f2278b29018897
Gilbeck <grael1302@gmail.com>
2026-09-02 13:09:39 +0700

    Pertemuan 3 - Operator Struktur Kendali

---
+++ Pertemuan3-Operator-Struktur-Kendali/Form1.vb
@@ -0,0 +1,40 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub btnInput_Click(sender As Object, e As EventArgs) Handles btnInput.Click
+        Dim nilaiUjian As Integer
+
+        If Not Integer.TryParse(txtNilai.Text, nilaiUjian) Then
+            MessageBox.Show("Masukkan dalam bentuk angka")
+            txtNilai.Focus()
+            Return
+        End If
+
+        If nilaiUjian < 0 OrElse nilaiUjian > 100 Then
+            MessageBox.Show("Masukkan nilai antara 0 sampai 100")
+            txtNilai.Focus()
+            Return
+        End If
+
+        If nilaiUjian <= 50 Then
+            picImage.Image = Image.FromFile("Assets\1.jpg")
+        End If
+
+        If nilaiUjian <= 75 Then
+            picImage.Image = Image.FromFile("Assets\2.png")
+
+        Else
+            picImage.Image = Image.FromFile("Assets\3.jpg")
+        End If
+
+
+    End Sub
+
+    Private Sub txtNilai_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtNilai.KeyPress
+        If Not Char.IsControl(e.KeyChar) AndAlso Not Char.IsDigit(e.KeyChar) Then
+            e.Handled = True
+        End If
+    End Sub
+End Class
